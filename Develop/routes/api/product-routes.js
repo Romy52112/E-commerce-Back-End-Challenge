3708d191bdecc3def97c603a31f1e75ba1c08488
Romy Pisigan <romymortilpisigan@gmail.com>
2024-05-10 02:22:37 -0700

    still trying to find the problem with the product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -18,15 +18,14 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
-
-    if (!productData) {
+   if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
